da2fe3f8414d6da0ec9e716b9086541bac2068d5
Nihad <nihad.nemetli@gmail.com>
2026-08-02 22:54:12 +0400

    add: structs created

--- include/engine.h
+++ include/engine.h
@@ -1,2 +1,15 @@
 #include <SDL3/SDL.h>
 #include <stdbool.h>
+
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    const char* title;
+    int width;
+    int height;
+} Screen;
+
+typedef struct {
+    Screen screen;
+    bool is_running;
+} Engine;
