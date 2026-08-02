fce671fc161269935cc88f682a3db86273135f1b
Nihad <nihad.nemetli@gmail.com>
2026-08-02 22:22:37 +0400

    new: CmakeLists.txt file created

---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/link.d"
+  "CMakeFiles/game.dir/src/main.c.o"
+  "CMakeFiles/game.dir/src/main.c.o.d"
+  "bin/game"
+  "bin/game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
